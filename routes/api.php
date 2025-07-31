15df68e26d6c0746ba6c298f69cefd17d40f8cdc
Drago Vlacic <dragomirvlacic@gmail.com>
2025-08-01 00:31:36 +0200

    add authentication and authorization(via tokens)

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,21 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TeamController;
 use App\Http\Controllers\PlayerController;
+use App\Http\Controllers\AuthController;
 
-Route::middleware('api')->group(function () {
-    // Resource ruta (REST konvencija)
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::get('teams/{team}/players', [TeamController::class, 'players']);
+
+Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('teams', TeamController::class);
     Route::apiResource('players', PlayerController::class);
-    Route::get('teams/{team}/players', [TeamController::class, 'players']);
 
     Route::post('teams/{team}/add-player', [TeamController::class, 'addPlayer']);
-
     Route::delete('teams/{team}/remove-player/{player}', [TeamController::class, 'removePlayer']);
 
+    Route::post('logout', [AuthController::class, 'logout']);
 });
